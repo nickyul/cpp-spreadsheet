43ec19baa0b7ee7d6d2f05971d5cb6031b858542
nickyul <156240952+nickyul@users.noreply.github.com>
2024-09-20 13:55:35 +0300

    Update sheet.h

--- spreadsheet/sheet.h
+++ spreadsheet/sheet.h
@@ -1,1 +1,40 @@
-/* разместите здесь свой код */
+#pragma once
+
+#include "cell.h"
+#include "common.h"
+
+#include <memory>
+#include <functional>
+#include <vector>
+
+using CellPtr = std::unique_ptr<Cell>;
+
+class Sheet : public SheetInterface {
+public:
+    explicit Sheet();
+
+    ~Sheet();
+
+    void SetCell(Position pos, std::string text) override;
+
+    const CellInterface* GetCell(Position pos) const override;
+    CellInterface* GetCell(Position pos) override;
+
+    const Cell* GetSimplyCell(Position pos) const;
+    Cell* GetSimplyCell(Position pos);
+
+    void ClearCell(Position pos) override;
+
+    Size GetPrintableSize() const override;
+
+    void PrintValues(std::ostream& output) const override;
+    void PrintTexts(std::ostream& output) const override;
+
+private:
+
+    void SetColSize();
+    void SetRowSize();
+
+    Size table_size_;
+    std::vector<std::vector<CellPtr>> cell_pointers_;
+};
